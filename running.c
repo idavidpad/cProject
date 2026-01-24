3e8a8d225633cc075aecd17f4b2ac8eb883f9743
idavidWin <idavidpad@gmail.com>
2026-01-24 14:44:14 +0800

    add mac 独有的学习文件

--- running.c
+++ running.c
@@ -2,8 +2,7 @@
 const int S_PER_M = 60;
 const int S_PER_H = 3600;
 const double M_PER_K = 0.62137;
-
-int main(void) {
+const..int main(void) {
     double distk, distm;
     double rate;
     int min, sec;
