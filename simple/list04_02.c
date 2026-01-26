7630bec33d871e87679ce1d96ea1d3d0101110af
iMacbook <idavidpad@gmail.com>
2026-01-26 21:20:41 +0800

    add list04_02.c

---
+++ simple/list04_02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    int num1, num2;
+    int sum = 0;
+
+    printf("请输入两个数\n");
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+
+    if (num1 < num2) {
+        sum = num1;
+        while (num1 <= sum && sum < num2) {
+            num1++;
+            sum += num1;
+        }
+    }
+
+    printf("sum is: %d\n", sum);
+    return 0;
+}
