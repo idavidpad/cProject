dc487c9b116b685a5da69baeceb4086c5e8da822
idavidWin <idavidpad@outlook.com>
2026-01-30 11:16:17 +0800

    add for_none.c

---
+++ for_none.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int ans, n;
+    ans = 2;
+
+    for (n = 3; ans <= 25;) {
+        ans = ans * n;
+    }
+
+    printf("n = %d; ans = %d.\n", n, ans);
+    return EXIT_SUCCESS;
+}
