147ff68865eedc5171db33d88726fbf553354984
iMacbook <idavidpad@gmail.com>
2026-02-02 23:09:50 +0800

    调试的配置成功了。

--- main.c
+++ main.c
@@ -7,8 +7,6 @@ int main(void) {
 	printf("hello main1.1\n");
 	puts("hello main1.2");
 
-	printf("hello main2\n");
-	printf("modify from dev-vim\n");
 	printf("hehe, I'm here, I is dev-david\n");
 	return 0;
 }
