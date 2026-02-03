1d925eec030b8fd6bb561b2d188f48912463fbb8
iMacbook <idavidpad@gmail.com>
2026-02-03 19:47:31 +0800

    add showchar1.c and showchar2.c

---
+++ src/showchar1.c
@@ -0,0 +1,28 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+void display(char cr, int lines, int width);
+
+int main(int argc, char *argv[]) {
+	int ch;
+	int rows, cols;
+	printf("Enter a character and two integers:\n");
+	while ((ch = getchar()) != '\n') {
+		scanf("%d %d ", &rows, &cols);
+		display(ch, rows, cols);
+		printf("Enter another character and two integers;\n");
+		printf("Enter a newline to quit.\n");
+	}
+	printf("Bye!\n");
+	return EXIT_SUCCESS;
+}
+
+void display(char cr, int lines, int width) {
+	int row, col;
+	for (row = 1; row <= lines; row++) {
+		for (col = 1; col <= width; col++) {
+			putchar(cr);
+		}
+		putchar('\n');
+	}
+}
