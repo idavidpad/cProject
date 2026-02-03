41f044ca635ab81b593ae6207e1dcf928ffebc8f
iMacbook <idavidpad@gmail.com>
2026-02-03 22:02:52 +0800

    add checking.c

---
+++ src/checking.c
@@ -0,0 +1,90 @@
+// checking.c -- 输入验证
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+long get_long(void);
+bool bad_limits(long begin, long end, long low, long high);
+double sum_squares(long a, long b);
+
+int main(int argc, char *argv[]) {
+	const long MIN = -1000000L;
+	const long MAX = +1000000L;
+	long start;
+	long stop;
+	double answer;
+
+	printf(
+		"This program computes the sum of the squares of"
+		"integers in a range.\nThe lower bound should not"
+		"be less than -1000000 and \nthe upper bound "
+		"should not be more than +1000000.\nEnter the "
+		"limits (enter 0 for both limits to quit):\n"
+		"lower limit: ");
+	start = get_long();
+	printf("upper limit: ");
+	stop = get_long();
+
+	while (start != 0 || stop != 0) {
+		if (bad_limits(start, stop, MIN, MAX)) {
+			printf("Please try again.\n");
+		} else {
+			answer = sum_squares(start, stop);
+			printf("The sum of the squares from %ld to %ld is %g\n", start,
+				   stop, answer);
+		}
+		printf(
+			"Enter the limits (enter 0 for both limits to quit):\n"
+			"lower limit: ");
+		start = get_long();
+		printf("upper limit: ");
+		stop = get_long();
+	}
+
+	printf("Done.\n");
+	return EXIT_SUCCESS;
+}
+
+long get_long(void) {
+	long input;
+	char ch;
+
+	while (scanf("%ld", &input) != 1) {
+		while ((ch = getchar()) != '\n') {
+			putchar(ch);
+		}
+		printf(" is not an integer.\nPlease enter an ");
+		printf("integer value, such as 25, -178, or 3:  ");
+	}
+
+	return input;
+}
+
+double sum_squares(long a, long b) {
+	double total = 0;
+	long i;
+
+	for (i = a; i <= b; i++) {
+		total += (double)(i * i);
+	}
+
+	return total;
+}
+
+bool bad_limits(long begin, long end, long low, long high) {
+	bool not_good = false;
+
+	if (begin >= end) {
+		printf("%ld is not smaller than %ld.\n", begin, end);
+		not_good = true;
+	}
+	if (begin < low || end < low) {
+		printf("Values must be %ld or greater.\n", low);
+		not_good = true;
+	}
+	if (begin > high || end > high) {
+		printf("Values must be %ld or less.\n", high);
+		not_good = true;
+	}
+
+	return not_good;
+}
