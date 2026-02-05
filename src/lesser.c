d324029fc0716bbf35f04a4a1fcdf0b757519e9c
wsl <idavidpad@126.com>
2026-02-05 08:41:22 +0800

    add lesser.c

---
+++ src/lesser.c
@@ -0,0 +1,28 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int imin(int, int);
+
+int main(int argc, char* argv[]) {
+	int evil1, evil2;
+	printf("Enter a pair of integers (q to quit): \n");
+	while (scanf("%d %d", &evil1, &evil2) == 2) {
+		printf("The lesser of %d and %d is %d.\n", evil1, evil2,
+			   imin(evil1, evil2));
+		printf("Enter a pair of integer (q to quit):\n");
+	}
+
+	printf("Bye!\n");
+	return EXIT_SUCCESS;
+}
+
+int imin(int n, int m) {
+	int min;
+	if (n < m) {
+		min = n;
+	} else {
+		min = m;
+	}
+	return min;
+}
