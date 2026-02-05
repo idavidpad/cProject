392e2e33d5320927c16f02a53902fef503cd0784
wsl <idavidpad@126.com>
2026-02-05 09:33:10 +0800

    add proto.c

---
+++ src/proto.c
@@ -0,0 +1,14 @@
+// proto.c -- 使用函数原型
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+int imax(int, int);
+
+int main(int argc, char* argv[]) {
+	printf("The maximum of % d and %d is %d.\n", 3, 5, imax(3, 5));
+	printf("The maximum of % d and %d is %d.\n", 3, 5, imax(3.5, 3.5));
+
+	return EXIT_SUCCESS;
+}
+
+int imax(int n, int m) { return (n > m) ? n : m; }
