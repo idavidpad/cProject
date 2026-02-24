a873029be2f14f5a80e4fcfc3e7958fe223bacef
iMacbook <idavidpad@gmail.com>
2026-02-24 17:30:06 +0800

    add fig02_13.c

---
+++ src/fig02_13.c
@@ -0,0 +1,33 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	printf("Enter two integers, and I will tell you\n");
+	printf("the relationships they satisfy: \n");
+
+	int num1;
+	int num2;
+
+	scanf("%d %d", &num1, &num2);
+
+	if (num1 == num2) {
+		printf("%d is equal to %d\n", num1, num2);
+	}
+	if (num1 != num2) {
+		printf("%d is not equal to %d\n", num1, num2);
+	}
+	if (num1 < num2) {
+		printf("%d is less than %d\n", num1, num2);
+	}
+	if (num1 > num2) {
+		printf("%d is greater than %d\n", num1, num2);
+	}
+	if (num1 <= num2) {
+		printf("%d is less than or equal to %d\n", num1, num2);
+	}
+	if (num1 >= num2) {
+		printf("%d is greater than or equal to %d\n", num1, num2);
+	}
+	return EXIT_SUCCESS;
+}
